1f07a2544d9bc80b7e4a0ab5689748984741b794
Kostiantyn <sinkkostia@gmail.com>
2024-11-19 14:33:43 +0200

    Updated model calculation, added tests for it

--- Simulating_the_replacement_of_cars/model/ModelMain.py
+++ Simulating_the_replacement_of_cars/model/ModelMain.py
@@ -1,11 +1,11 @@
 from Simulating_the_replacement_of_cars.model.Calculations import calculate_transition_matrix, simulate_changing
 
-years = 1
+years = 10
 initial_dvz_share = 0.8
 initial_ev_share = 0.2
 
-fuel_price = 1.2 # Ціна пального за 100 км
-electricity_price = 0.15 # Ціна електроенергії за 100 км
+fuel_price = 1.9 # Ціна пального за 100 км
+electricity_price = 0.75 # Ціна електроенергії за 100 км
 dvz_maintenance_factor = 1.0 # Коефіцієнт обслуговування ДВЗ
 ev_maintenance_factor = 0.8 # Коефіцієнт обслуговування електромобіля
 dvz_range = 600 # Дальність поїздки на ДВЗ
@@ -13,7 +13,7 @@ ev_range = 300 # Дальність поїздки на електромобіл
 charging_speed = 50 # Швидкість зарядки
 refueling_speed = 300 # Швидкість заправки
 ev_subsidy = 5 # Субсидія на електромобілі
-dvz_subsidy = 0 # Субсидія на ДВЗ
+dvz_subsidy = 1 # Субсидія на ДВЗ
 
 # Марковська матриця переходів, наприклад
 # P[DVZ->DVZ, DVZ->EV, EV->DVZ, EV->EV]
